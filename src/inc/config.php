feae220208f6934adeb06b311c046a13a12d587f
Joris Vos <jorisvos037@gmail.com>
2018-10-31 12:22:56 +0100

    Ignored inc/config.php

--- src/inc/config.php
+++ src/inc/config.php
@@ -5,11 +5,11 @@
     define('DBHOST','localhost');
     define('DBNAME','wideworldimporters');
     define('DBUSER','root');
-    define('DBPASS','');
+    define('DBPASS','root');
 
     $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
+
     // Define template location
     $template = "template.php";
 ?>
