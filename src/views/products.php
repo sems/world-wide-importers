4a1443a3a785abb14108f191119d344742097c84
Joris Vos <jorisvos037@gmail.com>
2018-11-06 11:22:15 +0100

    hoi

--- src/views/products.php
+++ src/views/products.php
@@ -6,46 +6,22 @@ if(sizeOf($arrayProducts) < 1) {
   $i = 0;
   print("<div class='row'>");
   foreach ($arrayProducts as $row) {
-    //if ($i == 0) {
-      //print("<div class='row'>");
-    //}
-
     print("<div class='col-md-4 products-top-margin'>
       <div class='card'>
         <a class='products-link' href='product.php?id=".$row['StockItemID']."'>
-          <img class='card-img-top' src='data:image/gif;base64,".base64_encode($row['Photo'])."' />
+          ".(strlen($row['Photo']) < 1 ? "<img class='card-img-top' src='img/image_not_found.png' />":"<img class='card-img-top' src='data:image/gif;base64,".base64_encode($row['Photo'])."'/>")."
         </a>");
 
     print("<div class='card-body'>");
-    print("<a class='products-link' href='product.php?id=".$row['StockItemID']."'>
-      <span class='product-name'>".
-      $row['StockItemName'].
-      "</span></a><br /><span class='product-price'>".
+    print("<table><tr><td><a class='products-link' href='product.php?id=".$row['StockItemID']."'>"
+      .$row['StockItemName'].
+      "</a></td><td class='product-price'>".
       "&euro;".$row['UnitPrice'].
-      "</span>");
+      "</td></tr></table>");
     
     print("</div></div></div>");
-    //if ($i == 2) {
-      //print("</div>");
-      //$i = 0;
-      //continue;
-    //}
-    //$i++;
-
-    // print photo
-    //print("<a href='product.php?id=".$row['StockItemID']."'><img width='50px' src='data:image/gif;base64,".base64_encode($row['Photo'])."'/>");
-    //print name
-    //print($row['StockItemName'] . "</a><br />");
   }
   print("</div>");
 }
 
-?>
-
-
-<?php
-// // Use to validate photo extension
-// print("<script type='text/javascript'>
-//   (/\.(gif|jpg|jpeg|tiff|png)$/i).test(".$row['Photo'].")
-// </script>");
 ?>
