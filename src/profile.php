f283aef2989a513056fad89debb422bb03dcf446
Sem <semspanhaak@gmail.com>
2018-11-07 13:20:49 +0100

    filled account

--- src/profile.php
+++ src/profile.php
@@ -2,7 +2,10 @@
     require('inc/config.php');
     // title variable (used in template)
     $title = "Profiel";
-
+    if(!isset($_SESSION['logged_in']) || ($_SESSION['logged_in'] == false)) {
+        header('Location: login.php');
+        exit();
+    } 
     $view = "profile.php";
     
     include_once $template;
--- src/views/profile.php
+++ src/views/profile.php
@@ -1,11 +1,34 @@
-<?php
-
-if(!isset($_SESSION['logged_in']) || ($_SESSION['logged_in'] == false)) {
-    header('Location: login.php');
-    exit();
-} else {
-    $loggedInMess = 'Welkom '.$_SESSION['user'].', wat leuk dat je er weer bent.';
-    echo($loggedInMess);
-}
-
-?>
+<div class="row">
+    <div class="col-md-12">
+        <?php
+            $loggedInMess = '<h4>Welkom <strong>'.$_SESSION['user'].'</strong></h4>';
+            echo($loggedInMess);
+        ?>
+    </div>
+    <div class="card-deck">
+        <div class="card">
+          <img class="card-img-top" src="img/image_not_found.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Mijn adresen</h5>
+            <p class="card-text">Wijzig hier de adressen die je hebt indegesteld in je account.</p>
+            <a href="#" class="btn btn-primary">Naar adressen</a>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="img/image_not_found.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Mijn orders</h5>
+            <p class="card-text">Hier vindt je alle informatie over je bestellingen.</p>
+            <a href="#" class="btn btn-primary">Naar orders</a>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="img/image_not_found.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Account instellingen</h5>
+            <p class="card-text">Wijzig hier alle instellingen binnen je account.</p>
+            <a href="#" class="btn btn-primary">Naar instellingen</a>
+          </div>
+        </div>
+    </div>
+</div>
