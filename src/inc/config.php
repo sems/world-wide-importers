d71c94e52dc128eea46dde192f0a0a0f2ee94a66
Sem <semspanhaak@gmail.com>
2018-11-19 12:48:09 +0100

    included functions

--- src/inc/config.php
+++ src/inc/config.php
@@ -1,11 +1,11 @@
 <?php
     session_start();
-
+    include("functions.php");
     // Database credentials
     define('DBHOST','localhost');
     define('DBNAME','wideworldimporters');
     define('DBUSER','root');
-    define('DBPASS','root');
+    define('DBPASS','');
 
     $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
