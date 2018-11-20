dffd0ade6f02575b7e94c4228ed44a6961bc44fa
Joris Vos <jorisvos037@gmail.com>
2018-11-20 13:55:51 +0100

    Fixed product page(made it look nicer)

---
+++ src/contact.php
@@ -0,0 +1,12 @@
+<?php
+    require('inc/config.php');
+
+    // Define title variable
+    $title = "Contact";
+
+    // Defining view location
+    $view = "contact.php";
+
+    // Include template
+    include_once $template;
+?>
--- src/views/product.php
+++ src/views/product.php
@@ -3,15 +3,24 @@
         // Check if a product is selected, if not print error message
         print('<div class="alert alert-danger" role="alert">Er is geen product geselecteerd!</div>');
     } else {
-        print("<table><tr>");
-        print("<td>".(strlen($product['Photo']) < 1 ? "<img id='productImage' src='img/image_not_found.png' />":"<img id='productImage' src='data:image/gif;base64,".base64_encode($product['Photo'])."'/>")."</td>");
-        print("<td>".$product['StockItemName']);
-        print("<br />".$product['UnitPrice']);
-        print((strlen($product['MarketingComments']) < 1 ? "":"<br />".$product['MarketingComments']));
-        print((strlen($product['Tags']) < 3 ? "":"<br />Tags: ".$product['Tags']));
-        print("<br />Op voorraad: ".$product['QuantityOnHand']);
-        print(($product['ColorID'] == 0 ? "":"<br />".$product['ColorName']));
-        print("<form method='post' action='f_add_to_basket.php'>
+        print("<div class='row'>");
+        print("<div class='col-md-3'>".(strlen($product['Photo']) < 1 ? "<img id='productImage' src='img/image_not_found.png' />":"<img id='productImage' src='data:image/gif;base64,".base64_encode($product['Photo'])."'/>")."</div>");
+        print("<div class='col-md-5'>
+                    <div class='card'>
+                        <div class='card-header'>
+                            <strong>".$product['StockItemName']."</strong>
+                        </div>");
+        print("<div class='card-body'>
+                    <h6 class='card-subtitle mb-2'>
+                        Prijs: &euro;".$product['UnitPrice']."
+                    </h6>
+                    <p class='card-text'>
+                        <table><tr><td>");
+        print((strlen($product['MarketingComments']) < 1 ? "":"Opmerkingen: </td><td>".$product['MarketingComments']."</td></tr><tr><td>"));
+        print("Voorraad: </td><td>".$product['QuantityOnHand']."</td></tr><tr><td>");
+        print((strlen($product['Tags']) < 3 ? "":"Tags: </td><td>".$product['Tags']."</td></tr><tr><td>"));
+        print(($product['ColorID'] == 0 ? "":"Kleur: </td><td>".$product['ColorName']."</td></tr><tr><td>"));
+        print("</td></tr></table></p><form method='post' action='f_add_to_basket.php'>
                 <div>
                     <input style='float: left;' class='form-control col-md-4' type='number' name='itemAmount' id='itemAmount' min='1' max='".$product['QuantityOnHand']."' value='1'>
                     <input type='hidden' id='".$product['StockItemID']."' name='itemID' value='".$product['StockItemID']."'>
@@ -20,7 +29,6 @@
                     </button>
                 </div>
             </form>");
-        print("</td>");
-        print("</tr></table>");
+        print("</div></div></div>");
     }
 ?>
