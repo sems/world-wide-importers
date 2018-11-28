381999ba16e0a36e9b1788089c4b0b2fefe72e9e
Sem <semspanhaak@gmail.com>
2018-11-28 10:22:01 +0100

    restart paidment

--- src/forms/f_restart_payment.php
+++ src/forms/f_restart_payment.php
@@ -2,6 +2,7 @@
     require('inc/config.php');
     
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        
         $orderId = $_POST['orderId'];
         try {
             // This query needs to be to one on customertransactions
@@ -14,45 +15,42 @@
             header('Location: payment.php');
         };
 
-        if($_POST['payment'] == 'open') {
-            # check if payment is open trough form
+        // if($_POST['payment'] == 'open') {
+        //     # check if payment is open trough form
 
-            // in format date("Y-m-d H:i:s");
-            $madeDate = $invoice['LastEditedWhen'];
-            $within10Mark = date('Y-m-d H:i:s', strtotime($madeDate. ' + 10 minutes'));
-            $currentDate = date('Y-m-d H:i:s');
+        //     // in format date("Y-m-d H:i:s");
+        //     $madeDate = $invoice['LastEditedWhen'];
+        //     $within10Mark = date('Y-m-d H:i:s', strtotime($madeDate. ' + 10 minutes'));
+        //     $currentDate = date('Y-m-d H:i:s');
 
-            if ($currentDate < $within10Mark) {
-                # payment is within 10 min
-                $paymentUrl = $invoice['Comments'];
-                header("Location: " . $paymentUrl, true, 303);
-            }
-        } 
+        //     if ($currentDate < $within10Mark) {
+        //         # payment is within 10 min
+        //         $paymentUrl = $invoice['Comments'];
+        //         header("Location: " . $paymentUrl, true, 303);
+        //     }
+        // } 
+
         # make new call.
-
-        print('new call');
+        
         $_SESSION['orderid'] = $orderId;
         $_SESSION['invoiceID'] = $invoice['InvoiceID'];
 
         try {
-            $orderlineStmt = $db->prepare("SELECT UnitPrice FROM Orderlines WHERE OrderID=:order_id");
+            $orderlineStmt = $db->prepare("SELECT Quantity, UnitPrice FROM Orderlines WHERE OrderID=:order_id");
             $orderlineStmt->execute(['order_id' => $orderId]);
-            $orderlines = $orderlineStmt->fetch(); 
+            $orderlines = $orderlineStmt->fetchAll(); 
         } catch (PDOException $e) {
             //Gives the error message if possible.
             setAlert("Error.", "danger", $e->getMessage());
-            header('Location: payment.php');
+            header('Location: order.php?id='.$orderId);
         };
 
         $total = 0;
-        foreach ($orderlines as $singleLine) {
-            var_dump($singleLine);
-            $total += $singleLine['UnitPrice'];
+
+        foreach ($orderlines as $line) {
+            $total += $line['Quantity']*$line['UnitPrice'];
         }
-        print('$total '.$total);
-        // Are needed 
-        // $amount = $_SESSION['totalprice'];
-        // $orderID = $_SESSION['orderid'];
-        // $invoiceID = $_SESSION['invoiceID'];
+        $_SESSION['totalprice'] = number_format($total, 2);
+        header('Location: payment.php');
     } 
 ?>
