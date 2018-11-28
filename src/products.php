0f5d22975a42cc113127f3f7fb0d2d64ac25866f
Furkan Akmese <iamfurkanakmese@gmail.com>
2018-11-28 10:22:43 +0100

    fixed bug 3.0

--- src/products.php
+++ src/products.php
@@ -66,7 +66,7 @@
             }
           }
         }
-        $totalPages = round($arrayProducts[0]['total'] / $resultsPerPage);
+        $totalPages = $arrayProducts[0]['total'] / $resultsPerPage;
         echo $totalPages;
         $sql = 'SELECT * FROM stockitems si JOIN stockitemstockgroups sisg ON sisg.StockItemID = si.StockItemID JOIN stockgroups sg ON sg.StockGroupID = sisg.StockGroupID WHERE si.StockItemName LIKE "%'.$search.'%" AND sg.StockGroupName LIKE "%'.$request.'%" LIMIT '.$start.', '.$resultsPerPage.' '.(isset($_GET['order']) ? "ORDER BY si.UnitPrice ".$order."" : "").'';
       } else {
@@ -90,7 +90,7 @@
           }
         }
       }
-      $totalPages = round($arrayProducts[0]['total'] / $resultsPerPage);
+      $totalPages = $arrayProducts[0]['total'] / $resultsPerPage;
 
       $sql = 'SELECT * FROM stockitems si JOIN stockitemstockgroups sisg ON sisg.StockItemID = si.StockItemID JOIN stockgroups sg ON sg.StockGroupID = sisg.StockGroupID WHERE sg.StockGroupName LIKE "%'.$request.'%" LIMIT '.$start.', '.$resultsPerPage.' '.(isset($_GET['order']) ? "ORDER BY si.UnitPrice ".$order."" : "").'';
     }
@@ -113,7 +113,7 @@
             }
           }
         }
-        $totalPages = round($arrayProducts[0]['total'] / $resultsPerPage);
+        $totalPages = $arrayProducts[0]['total'] / $resultsPerPage;
 
         $sql = 'SELECT * FROM stockitems si JOIN stockitemstockgroups sisg ON sisg.StockItemID = si.StockItemID JOIN stockgroups sg ON sg.StockGroupID = sisg.StockGroupID WHERE si.StockItemName LIKE "%'.$search.'%" AND sg.StockGroupName LIKE "%'.$request.'%"'.(isset($_GET['order']) ? "ORDER BY si.UnitPrice ".$order."" : "").' LIMIT '.$start.', '.$resultsPerPage.'';
       } else {
@@ -133,7 +133,7 @@
             }
           }
         }
-        $totalPages = round($arrayProducts[0]['total'] / $resultsPerPage);
+        $totalPages = $arrayProducts[0]['total'] / $resultsPerPage;
 
         $sql = 'SELECT * FROM stockitems si JOIN stockitemstockgroups sisg ON sisg.StockItemID = si.StockItemID JOIN stockgroups sg ON sg.StockGroupID = sisg.StockGroupID WHERE sg.StockGroupName LIKE "%'.$request.'%" '.(isset($_GET['order']) ? "ORDER BY si.UnitPrice ".$order."" : "").' LIMIT '.$start.', '.$resultsPerPage.'';
       }
@@ -157,8 +157,7 @@
           }
         }
       }
-      $totalPages = round($arrayProducts[0]['total'] / $resultsPerPage);
-
+      $totalPages = $arrayProducts[0]['total'] / $resultsPerPage;
       $sql = 'SELECT * FROM stockitems WHERE SearchDetails LIKE "%'.$request.'%" LIMIT '.$start.', '.$resultsPerPage.' ';
     }
   } else {
@@ -176,7 +175,7 @@
         }
       }
     }
-    $totalPages = round($arrayProducts[0]['total'] / $resultsPerPage);
+    $totalPages = $arrayProducts[0]['total'] / $resultsPerPage;
 
     $sql = 'SELECT StockItemID, StockItemName, Photo, UnitPrice FROM stockitems  LIMIT '.$start.', '.$resultsPerPage.' ';
   }
