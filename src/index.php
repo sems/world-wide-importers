c553c22d4540be31050ad16a28803729d80c357a
Joris Vos <jorisvos037@gmail.com>
2018-12-05 11:40:35 +0100

    Edited index page

--- src/index.php
+++ src/index.php
@@ -9,6 +9,32 @@
     // Defining view location
     $view = "index.php";
 
+    $products_sql =  'SELECT * 
+                        FROM stockitems
+                        WHERE StockItemID IN (64, 152, 92, 17, 166, 1, 38, 143)';
+    
+    $arrayProducts = array();
+    
+    if (strlen($products_sql) < 1 == false) {
+        /*
+        * Prepare query
+        */
+        $query = $db->prepare($products_sql);
+    
+        /*
+        * Query is being executed, results (rows) are being counted and if that is above 0
+        * the results will be pushed to the arrayProducts, these are displayed in the view
+        */
+        if($query->execute()) {
+          $rowCount = $query->rowCount();
+          if($rowCount !== 0) {
+            while($products = $query->fetch()) {
+                array_push($arrayProducts, $products);
+            }
+          }
+        }
+      }
+
     // Include template
     include_once $template;
 ?>
--- src/views/index.php
+++ src/views/index.php
@@ -1,3 +1,40 @@
 <div class="jumbotron jumbotron-fluid bg-img">
     <img src="img/homepage-logo.png" />
 </div>
+
+<div class="row">
+    <h1 class="container">Uitgelicht</h1>
+    <?php
+        // For each product print product photo (or fallback photo) and add product name
+        if(sizeOf($arrayProducts) >= 1) {
+        $i = 0;
+        print("<div class='card-deck product-row-margin'>");
+        foreach ($arrayProducts as $row) {
+            print("<div class='col-md-3 products-top-margin '>
+            <div class='card'>
+                <a class='products-link' href='product.php?id=".$row['StockItemID']."'>
+                ".(strlen($row['Photo']) < 1 ? 
+                    "<img class='card-img-top' src='img/image_not_found.png' />":
+                    "<img class='card-img-top' src='data:image/gif;base64,".base64_encode($row['Photo'])."'/>")."
+                </a>");
+
+            print("<div class='card-body'>");
+            print("<table><tr><td><a class='products-link' href='product.php?id=".$row['StockItemID']."'>"
+            .$row['StockItemName'].
+            "</a></td>
+            <td class='product-price'>
+            &euro;".$row['UnitPrice'].
+            "</td></tr></table>");
+
+            print("</div></div></div>");
+        }
+        print("</div>");
+        } else { ?>
+
+        <div class="col-md-12">
+            <h3>Geen resultaten</h3>
+        </div>
+        <?php
+        }
+    ?>
+</div>
